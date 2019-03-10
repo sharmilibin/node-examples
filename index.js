936c8adc99e592bfc774b2423768da3244b349c9
Sharmi Libin <sharmi.m.rose@gmail.com>
2019-03-10 16:18:35 -0600

    node callback with error handling

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var rect=require('./rectangle');
 function solveRect(l,b)
 {
     console.log('solving the tectangle with '+l+ 'and'+b);
-    if (l<=0 || b<=0)
-    {
-        console.log('dimension should be greater than zero');
-    }
-    else{
-        console.log('perimeter of rectangle is :'+rect.perimeter(l,b));
-        console.log('Area of Rectangle is :' +rect.area(l,b));
-    }
-}
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+            console.log("erroe ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("after the call to rect()");
+};
 solveRect(2,5);
 solveRect(0,5);
 solveRect(-4,6);
