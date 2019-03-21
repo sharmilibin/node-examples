7bd12381cd6fde242fc263bc45aa501c084dbcd7
Sharmi Libin <sharmi.m.rose@gmail.com>
2019-03-21 16:35:06 -0600

    call back revisit

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var rect=require('./rectangle');
 function solveRect(l,b)
 {
-    console.log('solving the tectangle with '+l+ 'and'+b);
+    console.log('solving the rectange ' +l+ 'and' +b);
     rect(l,b, (err,rectangle) => {
-        if (err) {
-            console.log("erroe ", err.message);
-	    }
-        else {
-            console.log("The area of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.area());
-            console.log("The perimeter of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.perimeter());
+        if(err)
+        {
+            console.log('Error',err.message);
+        }
+        else{
+            console.log('The are of the rectangle is l=' +l+'and b=' +b+ 'is' +rectangle.area());
+            console.log('The are of the rectangle is l=' +l+'and b=' +b+ 'is' +rectangle.perimeter());
         }
     });
-    console.log("after the call to rect()");
+    console.log('after call rect()');
 };
 solveRect(2,5);
 solveRect(0,5);
